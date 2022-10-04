22705cc0da3f287cfba4b003d5c7280f6dbedae7
CesarAugustoNavasSerrano <cesarans@hotmail.com>
2022-10-03 22:13:25 -0500

    Variables PHP

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 addEventListener("DOMContentLoaded", async()=>{
-    let peticion = await fetch("https://cesaraugustonavasserrano.000webhostapp.com/Conexion_de_los_servidores_30_09_2022/api.php");
+    let peticion = await fetch("https://cesaraugustonavasserrano.000webhostapp.com/Variables_03_0_2022/api.php");
     let data = await peticion.text();
-    document.body.innerHTML = `<h1 style="background: red; color: #fff; font-family: Arial, Helvetica, sans-serif; padding: 10px; border-radius: 5px;">${data}</h1>`;
+    document.body.innerHTML = data;
 })
